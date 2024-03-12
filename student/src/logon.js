d119584f185b9c9e2abd56d17c13f1482308d389
Jozcefhs <jozcefhse@gmail.com>
2024-03-13 00:38:16 +0100

    hi

---
+++ student/src/logon.js
@@ -0,0 +1,125 @@
+import { initializeApp, deleteApp } from "firebase/app";
+import { getFirestore, collection, doc, getDoc, getDocs, query, where, and, or } from "firebase/firestore";
+import configs from "../../../src/JSON/configurations.json" assert {type: 'json'};
+
+// initial firebase app
+var app = initializeApp(configs[6])
+var db, JSSubjectRef, SSSubjectRef;
+
+const selectElt = document.querySelector('select#classroom');
+selectElt.addEventListener('change', (e) => {
+    deleteApp(app);
+    let optIndex = e.target.selectedIndex - 1;
+    app = initializeApp(configs[optIndex]);
+    // init services
+    db = getFirestore()
+    // collection refs
+})
+
+const classroom = document.querySelector('#classroom');
+const email = document.querySelector('#email');
+const password = document.querySelector('#password');
+const notice = document.querySelector('dialog#notice');
+const loginForm = document.forms.login;
+
+// document.forms.login.removeAttribute('data-disabled')
+
+loginForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    e.submitter.disable = true;
+    e.submitter.style.cursor = 'not-allowed';
+    const classroomCollectionRef = collection(db, 'students');
+    const q = query(classroomCollectionRef, and(or(where("email", "==", email.value), where("admission_no", "==", email.value)), where("password", "==", password.value)))
+    const querySnapshot = await getDocs(q);
+    // console.log(querySnapshot);
+    if(querySnapshot.empty) {
+        notice.querySelector('output').textContent = "Something's gone awry. Try again.";
+        notice.classList.add('active');
+        e.submitter.disabled = false;
+        e.submitter.style.cursor = 'pointer';
+    } else {
+        querySnapshot.docs.forEach(async doc => {
+            // let ue = doc.data().upload_enabled;
+            let photo_src = doc.data().photo_src;
+            let offered = doc.data().offered;
+            let snapshot = {
+                'id': doc.id,
+                'first_name': doc.data().first_name,
+                'last_name': doc.data().last_name,
+                'other_name': doc.data().other_name,
+                'gender': doc.data().gender,
+                'admission_no': doc.data().admission_no,
+                'arm': doc.data().arm,
+                'class': classroom.value,
+                'em': doc.data().email || doc.data().admission_no,
+                'pwd': doc.data().password,
+                photo_src,
+                offered,
+            }
+            //reset Configuration to fetch subjects
+            resetConfig();
+
+            if(photo_src && offered) {
+                sessionStorage.setItem('snapshot', JSON.stringify(snapshot));
+                return location.href = '../dist/index.html';
+            } else if (offered) {
+                sessionStorage.setItem('snapshot', JSON.stringify(snapshot));
+                return location.href = '../dist/temp.html?of=1&ps=0';
+            } else if (photo_src) {
+                const reservedSnapshot = classroom.value.startsWith('JSS') ? await getDoc(JSSubjectRef) : await getDoc(SSSubjectRef);
+                snapshot = {...snapshot, 'reservedPayload': reservedSnapshot.data() || reservedSnapshot.data()}
+                sessionStorage.setItem('snapshot', JSON.stringify(snapshot));
+                return location.href = '../dist/temp.html?of=0&ps=1';
+            } else {
+                const reservedSnapshot = classroom.value.startsWith('JSS') ? await getDoc(JSSubjectRef) : await getDoc(SSSubjectRef);
+                snapshot = {...snapshot, 'reservedPayload': reservedSnapshot.data()}
+                sessionStorage.setItem('snapshot', JSON.stringify(snapshot));
+                return location.href = '../dist/temp.html';
+            }
+        });
+    }
+})
+
+function resetConfig () {
+    deleteApp(app);
+    app = initializeApp(configs[6])
+    db = getFirestore();
+    JSSubjectRef = doc(db, "reserved", "2aOQTzkCdD24EX8Yy518");
+    SSSubjectRef = doc(db, "reserved", "eWfgh8PXIEid5xMVPkoq");
+}
+/*
+const mainRef = collection(db, "fileCollection");
+addDoc(collection(mainRef, "AGR", "JSS 1"),{
+*/
+
+// const mainRef = collection(db, "demo");
+// addDoc(collection(mainRef, "AGR", "subjectId"),{
+//     name: 'Konan Perry',
+//     type: 'Note'
+// })
+// .then(() => {
+//     console.log("Sent.")
+// })
+
+/*
+const note = query(collectionGroup(db, "subjectId"), where("type", "==", "Note"));
+const querySnapshot1 = await getDocs(note);
+querySnapshot1.docChanges().forEach((change) => {
+    if(change.type === "added") {
+        console.log("Document added.")
+    }
+    if (change.type === "modified") {
+        console.log("Document modified.")
+    }
+    if (change.type === "removed") {
+        console.log("Document removed.")
+    } else {
+        console.log("No document has changed since.")
+    }
+    console.log(change);
+})
+*/
+
+// querySnapshot1.forEach((doc) => {
+//     console.log(doc.id)
+// })
---
+++ student/src/test.js
@@ -0,0 +1,267 @@
+import { initializeApp, deleteApp } from "firebase/app";
+import { getFirestore, collection, doc, getDoc, setDoc, query, where, and, or, updateDoc } from "firebase/firestore";
+import configs from "../../../src/JSON/configurations.json" assert {type: 'json'};
+
+const params = atob(new URL(location.href).searchParams.get('sb'))
+const ct = parseInt(new URL(location.href).searchParams.get('ct'));
+let cat;
+
+const subby = JSON.parse(sessionStorage.getItem(params));
+const snappy = JSON.parse(sessionStorage.getItem('snapshot'));
+subby.forEach((sb, n) => {
+    if (sb.catNo == ct) {
+        cat = n;
+        // console.log(cat)
+        return;
+    }
+})
+// const cat = subby.length < ct - 1 ? subby.length - ct : subby.length - 1;
+
+const testAbbr = params;
+const testNum = ct - 1;
+const chosen = subby[cat].chosen;
+const choice = subby[cat].choice;
+const questions = subby[cat].questions;
+const rating = subby[cat].rating;
+const instr = subby[cat].instr;
+const code = subby[cat].code;
+const link = subby[cat].link + '#toolbar=0';
+
+const accDialog = document.querySelector('#accDialog');
+const chkDateDialog = document.querySelector('#chkDateDialog');
+const header = document.querySelector('header');
+const section = document.querySelector('section');
+const main = document.querySelector('main');
+// const dt = subby[cat].startDate;
+const startDate = new Date(subby[cat].startDate).setHours(24);
+const startTime = subby[cat].startTime;
+const duration = subby[cat].duration;
+
+// settingn up startTime
+/*
+const parts = startTime.split(':');
+const hr = parseInt(parts[0]);
+const mm = parseInt(parts[1]);
+var currentTime = new Date();
+currentTime.setHours(hr);
+currentTime.setMinutes(mm);
+currentTime.setMinutes(currentTime.getMinutes() + duration);
+var formattedTime = currentTime.getHours().toString().padStart(2, '0') + ':' + currentTime.getMinutes().toString().padStart(2, '0');
+*/
+
+async function chkDate() {
+    if (startDate < Date.now()) {
+        chkDateDialog.querySelector('output').innerHTML = 'This test has expired.<br>Tender any inquiries to your teacher.';
+        chkDateDialog.showModal();
+        return;
+    } else {
+        // display accDialog
+        accDialog.showModal();
+    }
+}
+await chkDate();
+
+var timer = document.getElementById('timer');
+function displayHeader() {
+    //display header resources
+    document.getElementById('person').textContent = `${snappy.last_name + ' ' + snappy.first_name + ' ' + snappy.other_name}`;
+    document.getElementById('task').textContent = snappy.offered[testAbbr];
+    timer.textContent = duration;
+    header.style.display = 'flex';
+    header.classList.remove('disp');
+}
+function displaySection() {
+    //display section resources
+    const times = document.querySelector('.sectionX');
+    instr.forEach((ins, i) => {
+        times.insertAdjacentHTML('beforebegin', `
+            <div>
+                <h2>0${i+1}</h2>
+                <p>${ins}</p>
+            </div>
+        `)
+    })
+    section.style.display = 'flex';
+    section.classList.remove('disp');
+}
+const iframe = document.querySelector('iframe');
+const tbody = document.querySelector('tbody');
+function displayMain() {
+    // populate answer sheet
+    let counter = 1;
+    let i, j;
+    for (i = 0; i < questions; i++) {
+        tbody.insertAdjacentHTML('beforeend', `
+            <tr>
+                <td>
+                    <label>${i+1}</label>
+                </td>
+            </tr>
+        `)
+        for (j = 0; j < choice; j++) {
+            tbody.lastElementChild.insertAdjacentHTML('beforeend', `
+                <td>
+                    <input type="radio" name="${i}" id="rd${counter}" value="${j+1}">
+                    <label for="rd${counter}">${String.fromCharCode(65+j)}</label>
+                </td>
+            `)
+            counter++;
+        }
+    }
+    main.style.display = 'flex';
+    main.classList.remove('disp');
+
+    // iframe.src = link;
+    const img = new Image();
+    img.onload = function () {
+        img.setAttribute('width', '100%');
+        iframe.contentDocument.body.appendChild(img);
+    }
+    img.src = link;
+}
+
+let intervalID;
+iframe.addEventListener('load', function () {
+    // console.log('Iframe has finished loading. Now start timer.')
+    // start timer
+})
+function countDown () {
+    if (timer.textContent == 0) {
+        clearInterval(intervalID);
+        submission();
+        // submitBtn.click();
+    } else {
+        timer.textContent -= 1;
+    }
+}
+
+// load options
+const uid = snappy.id;
+
+let buffer = new ArrayBuffer(questions);
+let dv = new DataView(buffer);
+let updateVal = [null, null, null, null];
+const msgDialog = document.querySelector('dialog#msgDialog');
+// const submitDialog = document.querySelector('dialog#submitDialog');
+const submitBtn = document.querySelector('.aside__footer input[type="submit"]');
+
+const accForm = document.forms.accForm;
+accForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const acc = accForm.acc.value;
+    // get test doc if available
+    const scoreRef = doc(db, "scores", uid);
+    await getDoc(scoreRef).then(res => {
+        /*
+        if (res.data()[testAbbr][testNum] != null) {
+            window.alert("You've already taken this test.");
+            return;
+        }*/
+        if (acc === code) {
+            accDialog.close();
+            document.documentElement.requestFullscreen();
+            displayHeader();
+            displaySection();
+            displayMain();
+            intervalID = setInterval(countDown, 1 * 60 * 1000);
+        } else {
+            window.alert("Invalid access token.")
+        }
+    })
+})
+
+// document.addEventListener('fullscreenchange', () => {
+//     if (document.fullscreenElement === null) {
+//         submission();
+//         // submitBtn.click();
+//     }
+// })
+
+const classIndex = configs[7].indexOf(snappy.class);
+
+// initial firebase app, assuming for SSS 3
+var app = initializeApp(configs[classIndex]);
+// init services
+var db = getFirestore();
+
+// const selectElt = document.querySelector('select#classroom');
+// selectElt.addEventListener('change', (e) => {
+//     deleteApp(app);
+//     let optIndex = e.target.selectedIndex - 1;
+//     app = initializeApp(configs[optIndex]);
+//     // init services
+//     db = getFirestore()
+//     // collection refs
+// })
+// lksHjPA7
+// F79pWGRz
+// sKi3qxLu
+// new Date(JSON.parse(sessionStorage.LIT)[0].startDate) > Date.now()
+// JSON.parse(sessionStorage.LIT)[0].link
+submitBtn.addEventListener('click', (e) => {
+    // display submitDialog
+    // submitDialog.showModal();
+    submission();
+});
+/*
+const yesBtn = document.querySelector('button#yes');
+yesBtn.addEventListener('click', (e) => {
+    submitDialog.querySelectorAll(button).forEach(btn => {
+        btn.disabled = true;
+        btn.style.cursor = 'not-allowed';
+    })
+    submission();
+    submitDialog.close();
+})
+*/
+async function submission() {
+    // submitBtn.addEventListener('click', async (e) => {
+        submitBtn.disabled = true;
+        submitBtn.style.cursor = 'not-allowed';
+        clearInterval(intervalID);
+    
+        let i, score = 0;
+        for (i = 0; i < questions; i++) {
+            if (chosen[i] == dv.getInt8(i)) {
+                score++;
+            }
+        }
+        const scoreRef = doc(db, "scores", uid)
+        // await getDoc(scoreRef).then( async res => {
+        //     if (res.data()?.[testAbbr] === undefined) {
+                updateVal.splice(testNum, 1, Number((score/questions*rating).toFixed(1)))
+                await setDoc(scoreRef, {
+                    [testAbbr]: updateVal,
+                }, { merge: true })
+                // console.log("Test updated 1.")
+                msgDialog.querySelector('output').innerHTML = `
+                    Your score:<br><large>${score} out of ${questions}</large>
+                `;
+                msgDialog.showModal();
+                tbody.style.pointerEvents = 'none';
+            // } else {
+            //     if (res.data()[testAbbr][testNum] != null) return window.alert("You've already taken this test.");
+                // let arr = res.data()[testAbbr];
+                // arr.forEach((element, index) => {
+                //     updateVal.splice(index, 1, element)
+                // });
+                // updateVal[testNum] = Number((score/questions*rating).toFixed(1));
+        
+                // await setDoc(scoreRef, {
+                //     [testAbbr]: updateVal,
+                // }, { merge: true })
+                // msgDialog.querySelector('output').innerHTML = `
+                //     Your score:<br><large>${score} out of ${questions}</large>
+                // `;
+                // msgDialog.showModal();
+                // tbody.style.pointerEvents = 'none';
+            // }
+        // })
+    
+    // })
+}
+
+const quizForm = document.forms.quizForm;
+quizForm.addEventListener('change', (e) => {
+    dv.setInt8(e.target.name, e.target.value);
+})
